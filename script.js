ff6425c4e5cd327b78fb59e81b0fa157ad1b9740
Prachi <prachipratyushaacharya@gmail.com>
2025-08-03 12:32:22 +0530

    up.3

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 var typed = new Typed (".text", {
-    strings: ["Frontend Devloper", "Graphic Designer", "UI/UX designer"],
-    typespeed:100,
-    backspeed: 100,
+    strings: ["Frontend Devloper", "Graphic Designer", "UI/UX Designer"],
+    typeSpeed:100,
+    backSpeed: 100,
     backDelay: 1000,
     loop: true
-
-})
+});
